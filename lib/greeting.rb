40590e025f60c51d14289fda1f3f877e04f1ad75
David Moore <hey@iamdavidmoore.com>
2018-04-06 20:10:24 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,3 +1,4 @@
 def greeting(name)
-  puts"Welcome, #{name}, to the 2001 Space Odyssey.  And welcome aboard the Discovery One!"
+  puts"Welcome, #{name}, to the 2001 Space Odyssey."
+  puts "It is nice to meet you!  And welcome aboard the Discovery One!"
 end
